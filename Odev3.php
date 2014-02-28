b47a3ed822d0c4f9c9a2a330fbdc7dd4bc66bdd5
BURCU <burcukolbay@gmail.com>
2014-02-28 15:17:19 +0200

    Create Odev3.php
    3.ödev/ creating random passwords

---
+++ Odev3.php
@@ -0,0 +1,39 @@
+<?php
+
+echo $sifre="basit sifre: " . basit_sifre_uret();
+echo "<br>";
+echo $sifre2="guclu sifre: " . guclu_sifre_uret();
+function basit_sifre_uret($uzunluk=10)
+{
+    $sifre="";
+    $karakter1="aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+    if($uzunluk<4)
+    {
+        echo "Sifre uzunlugu 3 karakterden fazla olmali";
+    }
+    else{
+    for( $i=1;$i<$uzunluk+1;$i++)
+    {
+       $sifre.=$karakter1{rand()%52};
+       
+    }
+    return $sifre;
+}}
+function guclu_sifre_uret($uzunluk=10)
+{
+    
+    $sifre2="";
+    $karakter2="aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ+-/?*!";
+    if($uzunluk<4)
+    {
+        echo "Sifre uzunlugu 3 karakterden fazla olmali";
+    }
+    else{
+    for($j=1;$j<$uzunluk+1;$j++)
+    {
+        $sifre2.=$karakter2{rand()%58};
+        
+    }
+    return $sifre2;
+    }
+}
